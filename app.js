7d700c325d5ae64e232e09302598e0e8b1f1236c
jethroallen22 <68844538+jethroallen22@users.noreply.github.com>
2020-09-25 18:41:50 +0800

    heroku

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.use(session({
 
 app.use(require("./controllers"))
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("now listening to port 3000")
 })
